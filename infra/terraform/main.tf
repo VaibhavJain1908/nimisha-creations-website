46bbe0acabaee74f3b768360fa1d58865843893e
Vaibhav Jain <64792363+VaibhavJain1908@users.noreply.github.com>
2026-06-12 16:40:41 +0530

    infra: add us-east-1 provider alias for ACM cert

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,6 +21,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 locals {
   project      = "nimisha-creations"
   bucket_name  = "nimisha-creations-website-prod"
